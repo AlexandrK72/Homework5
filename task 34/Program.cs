1cd73cc96767578f85e4b630bf78056ef08c1838
Alexandr <chika431sis@yandex.ru>
2023-10-05 00:45:10 +0500

    First commit

---
+++ task 34/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] RandomNumbersArray(int min, int max, int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+
+        arr[i] = rnd.Next(min, max + 1);
+
+    }
+    return arr;
+}
+
+
+int EvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+
+    }
+    return count;
+}
+
+
+
+void PrintArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (1000 < arr[i] && arr[i] < 99)
+        {
+            Console.WriteLine($"Ошибка. Введите положительное трехзначное число");
+        }
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+
+
+    }
+}
+
+
+bool CheckCorrectArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        // if (99 < arr[i] && arr[i] < 1000)
+        if ((arr[i]/10>0) && (arr[i]/100 >0) && (arr[i]/1000 == 0))
+        {
+            return true;
+        }
+        else Console.WriteLine($"Ошибка. Введите положительное трехзначное число");
+    return false;
+}
+
+
+
+
+int minnumber = Prompt("Введите миннимальное трехзначное положительное число = ");
+
+int maxnumber = Prompt("Введите максимальное трехзначное положительное число = ");
+
+
+int sizenumber = Prompt("Введите длину массива ");
+
+
+
+int[] array = RandomNumbersArray(minnumber, maxnumber, sizenumber);
+
+
+
+if (CheckCorrectArr(array))
+{
+    int evenNumbers = EvenNumbers(array);
+
+    Console.Write(" [");
+    PrintArray(array);
+    Console.Write("] -> ");
+    Console.WriteLine($"{evenNumbers}");
+}
+
+
